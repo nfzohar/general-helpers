80420c918a154f1a37a168b23c119ecfd3c875c1
Nejc Frece Žohar <47608726+nfzohar@users.noreply.github.com>
2025-06-13 18:40:45 +0200

    Update ApiCallHelper.ts

--- ApiCallHelper.ts
+++ ApiCallHelper.ts
@@ -1,28 +1,26 @@
 import axios from 'axios';
 import { get } from 'lodash';
 
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/',
-  withCredentials: true,
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN': get(
-      document.querySelector('meta[name="csrf-token"]'),
-      'content',
-    ),
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-});
+export default class ApiCallHelper {
+  // Default axios Instance, can be overwritten in contructor
+  private axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || '/',
+    withCredentials: true,
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-TOKEN': get(
+        document.querySelector('meta[name="csrf-token"]'),
+        'content',
+      ),
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  });
 
-class ApiCallHelper {
-  private static instance: ApiCallHelper;
-
-  constructor() {
-    if (!ApiCallHelper.instance) {
-      ApiCallHelper.instance = this;
+  constructor(axiosInstance: any) {
+    if (axiosInstance) {
+      this.axiosInstance = axiosInstance;
     }
-    return ApiCallHelper.instance;
   }
 
   async get(url: string) {
@@ -45,21 +43,38 @@ class ApiCallHelper {
     return this.request('delete', url);
   }
 
-  private async request(
-    method: 'get' | 'post' | 'put' | 'patch' | 'delete',
-    url: string,
-    data?: any,
-  ) {
+  private async request(method: string, url: string, data?: any) {
     try {
       const path = url?.includes('api/') ? url : `api/${url}`;
-      const response = await axiosInstance({ method, url: path, data });
+      let request = null;
 
-      return response.data;
+      if (method == 'get') {
+        request = this.axiosInstance.get(path);
+      }
+      if (method == 'post') {
+        request = this.axiosInstance.post(path, data);
+      }
+      if (method == 'put') {
+        request = this.axiosInstance.put(path, data);
+      }
+      if (method == 'patch') {
+        request = this.axiosInstance.patch(path, data);
+      }
+      if (method == 'delete') {
+        request = this.axiosInstance.delete(path);
+      }
+
+      if (!request) {
+        return null;
+      }
+
+      let result = await request.then((response) => {
+        return response?.data;
+      });
+
+      return result ?? null;
     } catch (error) {
       console.error(error?.response?.data || error?.message);
     }
   }
 }
-
-const apiCallHelper = new ApiCallHelper();
-export default apiCallHelper;
